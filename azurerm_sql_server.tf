dca1ca3a9aaaf6245cfb8560efb6b964546e72e1
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-05-15 18:36:25 -0400

    Create azurerm_sql_server.tf

---
+++ azurerm_sql_server.tf
@@ -0,0 +1,20 @@
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplesa"
+  resource_group_name      = azurerm_resource_group.mcit420zz5um.name
+  location                 = azurerm_resource_group.mcit420zz5um.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.mcit420zz5um.name
+  location                     = azurerm_resource_group.mcit420zz5um.location
+  version                      = "12.0"
+  
+
+  tags = {
+    environment = "production"
+  }
+}
